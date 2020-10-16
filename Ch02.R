a266fc07b8c63592b361e5d49b93c9517cd63eef
Hwayoung Yoon <ghkdud204@gmail.com>
2020-10-16 18:14:54 +0900

    Add files via upload

--- Ch02.R
+++ Ch02.R
@@ -54,24 +54,29 @@ mosaic(GenderGap, gp=shading_Friendly, residuals=chi.GG$stdres, residuals_type="
        labeling=labeling_residuals)
 
 # 가능도비 카이제곱 검정
+#### rJava install 참고 : https://r-pyomega.tistory.com/6
+Sys.setenv(JAVA_HOME='C:/Program Files/Java/jre1.8.0_261')
+install.packages("rJava")
+library(rJava)
 install.packages("Deducer")
 library(Deducer)
 likelihood.test(GenderGap)
 
 ## Section 2.5. 순서형 자료의 독립성 검정 
 Malform <- matrix(c(17066, 14464, 788, 126, 37, 48, 38, 5, 1, 1), ncol=2)
+install.packages("vcdExtra")
 library(vcdExtra)
 CMHtest(Malform, rscores = c(0, 0.5, 1.5, 4.0, 7.0)) #수준점수(rscores) 
   # Mantel의 스코어 검정 
+  # 대립가설 : 양의 상관관계를 갖는다. (단측검정. rho>0)
 (M <- sqrt(6.5699))
 (p.val <- 1- pnorm(M))
-  # 대립가설 : 양의 상관관계를 갖는다. 
 
 
 ## Section 2.6. 소표본 정확추론 
 # Fisher의 정확검정 
 tea <- matrix(c(3,1,1,3), ncol=2)
-fisher.test(tea)  # 양측검정 
+fisher.test(tea)  # 양측검정
 fisher.test(tea, alternative="greater") # 대립가설 : theta >1 
 
 # 중앙 p-값
